793560014ee8868ed15449c008dc7f07060ac481
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-09-02 01:08:30 -0300

    Aulas sobre Dataclasses,  Namedtuples e protocolo de Iterator

--- licoes/aula159.py
+++ licoes/aula159.py
@@ -5,16 +5,22 @@
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import dataclass
-
+from dataclasses import dataclass, field # asdict, astuple
 
 @dataclass
 class Pessoa:
-    nome: str
-    idade: int
-
+    nome: str = field(
+        default='MISSING', repr=False
+    )
+    sobrenome: str = 'Not sent'
+    idade: int = 100
+    # Não pode ter valores mutaveis nos constrtutores, por isso usamos field em dataclass
+    enderecos: list[str] = field(default_factory=list) 
 
 if __name__ == '__main__':
-    p1 = Pessoa('Luiz', 30)
-    p2 = Pessoa('Luiz', 30)
-    print(p1 == p2)
+    p1 = Pessoa('Gabriel', 'Maxsy')
+    # print(asdict(p1).keys())
+    # print(asdict(p1).values())
+    # print(asdict(p1).items())
+    # print(astuple(p1)[0])
+    print(p1)
