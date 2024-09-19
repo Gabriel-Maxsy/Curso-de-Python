e41795205ecccad774c0970755010630697b218e
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-09-18 23:47:33 -0300

    usando basico de requests

---
+++ licoes/aula191.py
@@ -0,0 +1,14 @@
+import requests
+
+# Se a url do site começar com "http://" siginifica que ele esta rodando na porta -> 80
+# Se a url do site começar com "https://" siginifica que ele esta rodando na porta -> 443 (google por exemplo)
+
+url = 'http://localhost:3333/'
+response = requests.get(url)
+
+print(response) # Se não especificar ele responde o status do "servidor/site"
+print(response.status_code) # Responde o status do "servidor/site" 
+print(response.headers) # Responde com o cabeçalho do "servidor"
+# print(response.content) # Responde com o conteudo em bytes do servidor
+# print(response.json()) Responderia com os dados que o servidor responder em json (o mais comum em sites externos)
+print(response.text) # Responde com o conteudo em bytes do servidor em si (neste caso de exemplo responderá o html)
