5ba8de7c6f7f23cfcfd21ce101025aef5a0b485b
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-11-21 00:59:00 -0300

    Organizando arquivos estaticos, criando blocos e organizando urls

--- licoes/aula207_django/blog/views.py
+++ licoes/aula207_django/blog/views.py
@@ -3,14 +3,25 @@ from django.shortcuts import render
 
 def blog(request):
     # Posso fazer o que eu quiser antes da resposta
+    
+    context = {
+        'text': 'Olá blog',
+        'title': 'titulo blog'
+    }
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context 
     )
 
 def exemplo(request):
     # Posso fazer o que eu quiser antes da resposta
+    context = {
+        'text': 'Olá exemplo',
+        'title': 'Pagina de exemplo'
+    }
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
     )
--- licoes/aula207_django/home/views.py
+++ licoes/aula207_django/home/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     # Posso fazer o que eu quiser antes da resposta
+    context = {
+        'text': 'pagina home',
+        'title': 'titulo da home'
+    }
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
     )
