499869c79636795f7643c51dd25b583d96915dae
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-11-24 23:41:59 -0300

    Fazendo cada post ser único

--- licoes/aula207_django/blog/urls.py
+++ licoes/aula207_django/blog/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 app_name = 'blog'
 
 # blog/
+# Django URLs:
+# https://docs.djangoproject.com/en/4.2/topics/http/urls/
 urlpatterns = [
     path('', views.blog, name='home'),
-    path('exemplo/', views.exemplo, name='exemplo')
+    path('<int:post_id>/', views.post, name='post'),
+    path('exemplo/', views.exemplo, name='exemplo'),
 ]
--- licoes/aula207_django/blog/views.py
+++ licoes/aula207_django/blog/views.py
@@ -1,17 +1,44 @@
 # from django.http import HttpResponse
+from typing import Any
 from django.shortcuts import render
+from blog.data import posts
+from django.http import HttpRequest
 
 def blog(request):
     # Posso fazer o que eu quiser antes da resposta
     
     context = {
-        'text': 'Olá blog',
-        'title': 'titulo blog'
+        # 'text': 'Olá blog',
+        'title': 'titulo blog',
+        'posts': posts
     }
     return render(
         request,
         'blog/index.html',
         context 
+    )
+
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+    
+    for post in posts:
+        
+        if post['id'] == post_id:
+            found_post = post
+            break
+    
+    if found_post is None:
+        raise Exception('Post não existe.')
+    
+    context = {
+        'post': found_post,
+        'title': found_post['title'] + ' - ',
+    }
+
+    return render(
+        request,
+        'blog/post.html',
+        context
     )
 
 def exemplo(request):
