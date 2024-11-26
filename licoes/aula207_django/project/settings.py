060395bc0a916ccd113c5663b3641b03a3f49284
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-11-25 23:20:12 -0300

    arquivo manage.py, arquivos estaticos, debug e allowed_hosts

--- licoes/aula207_django/project/settings.py
+++ licoes/aula207_django/project/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base' / 'static'
 ]
 
+# https://whitenoise.readthedocs.io/en/latest/
+STATIC_ROOT = BASE_DIR / 'static_files'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
